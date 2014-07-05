d42b97cb871b519d31be9e8f5028e57ed785d3cd
David Rodrigues <david.proweb@gmail.com>
2014-07-05 03:40:36 -0300

    [awk_path] Adicionado um cache de normalização de paths;

--- awk/engine/awk_path.php
+++ awk/engine/awk_path.php
@@ -2,6 +2,11 @@
 
 	// Responsável pela resolução e controle de paths.
 	class awk_path {
+		// Armazena os paths já normalizados.
+		// @type array<string, string>;
+		static private $normalized_cache = [
+		];
+
 		/** HELPERS */
 		// Normaliza um caminho absoluto quando o `php:realpath()` não for capaz.
 		// Nota: este método não resolverá [dot].
@@ -14,16 +19,21 @@
 				array_splice($path_parts, $path_filter - ( $k * 2 + 1 ), 2);
 			}
 
-			return str_replace("./", null, rtrim(join("/", $path_parts), "./"));
+			return self::$normalized_cache[$path] = str_replace("./", null, rtrim(join("/", $path_parts), "./"));
 		}
 
 		// Normaliza um caminho de arquivo ou pasta.
 		static public function normalize($path) {
+			// Se o caminho já foi normalizado, retorna.
+			if(isset(self::$normalized_cache[$path])) {
+				return self::$normalized_cache[$path];
+			}
+
 			// Se o caminho real existir, executará o processo através de um `php:preg_replace()`.
 			// Caso contrário, será necessário a utilização do método de normalização de *irreais*.
 			$realpath = realpath($path);
 			if($realpath) {
-				return preg_replace("/[\\\\\\/]+/", "/", $realpath);
+				return self::$normalized_cache[$path] = preg_replace("/[\\\\\\/]+/", "/", $realpath);
 			}
 
 			return self::normalize_unreal($path);
