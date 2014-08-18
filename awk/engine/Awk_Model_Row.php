eab155184ab411938e3c91c53df1e7f5b9cbe8c8
David Rodrigues <david.proweb@gmail.com>
2014-08-17 21:54:38 -0300

    [Awk_Model_Row] Adicionado métodos mágicos para acesso no registro (get, set, isset e unset);

--- awk/engine/Awk_Model_Row.php
+++ awk/engine/Awk_Model_Row.php
@@ -22,4 +22,25 @@
 		public function get_array() {
 			return $this->data;
 		}
+
+		/** MAGIC */
+		// Obtém uma informação do registro.
+		public function __get($key) {
+			return $this->data[$key];
+		}
+
+		// Redefine uma informação do registro.
+		public function __set($key, $value) {
+			$this->data[$key] = $value;
+		}
+
+		// Verifica se uma informação existe.
+		public function __isset($key) {
+			return array_key_exists($key, $this->data);
+		}
+
+		// Remove uma informação.
+		public function __unset($key) {
+			unset($this->data[$key]);
+		}
 	}
--- awk_suite/privates/asserts/Awk_Model.php
+++ awk_suite/privates/asserts/Awk_Model.php
@@ -34,3 +34,14 @@
 	// Executa uma query simples, e verifica seu resultado.
 	$result_row = $indirect_model->load_test();
 	$asserts->expect_equal($result_row->get_array(), [ "1" => "1" ]);
+
+	// Controla os dados de forma mágica.
+	$asserts->expect_equal($result_row->__get("1"), "1");
+	$asserts->expect_equal($result_row->__isset("1"), true);
+
+	$result_row->__unset("1");
+	$asserts->expect_equal($result_row->__isset("1"), false);
+
+	$result_row->test = "test";
+	$asserts->expect_equal($result_row->test, "test");
+	$asserts->expect_equal(isset($result_row->test), true);
