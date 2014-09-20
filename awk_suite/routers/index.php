2a73cd08aa0e7c75fff505c2b78329e4f5625501
David Rodrigues <david.proweb@gmail.com>
2014-09-20 16:50:08 -0300

    Removido arquivos que não serão mais úteis para a suite de testes;

--- awk_suite/routers/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	// Define a rota de raíz (apresentação da home).
-	$router->add_root("controller@suite::home_page");
-
-	// Inicia o processo de execução dos asserts.
-	$router->add_route("run/[awk->string @options]*", function($driver, $options) use($module) {
-		return $module->controller("suite")->home_run($options);
-	});
--- awk_suite/views/asserts/group.php
+++
@@ -1,4 +0,0 @@
-<div class="assert-group">
-	<div class="assert-group-title"><?php echo $title; ?></div>
-	<div class="assert-group-body"><?php echo $contents; ?></div>
-</div>
