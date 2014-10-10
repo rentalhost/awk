e661c1f8e1ec1d30eb533e95d3f32c6a70535ece
David Rodrigues <david.proweb@gmail.com>
2014-10-10 02:17:52 -0300

    Awk_Path: tornado a classe funcional como instância;

--- awk/engine/Awk_Path.php
+++ awk/engine/Awk_Path.php
@@ -1,11 +1,93 @@
 <?php
 
     /**
-     * Responsável pela resolução e controle de paths.
+     * Responsável pela definição e gerenciamento de caminhos de objetos como arquivos e diretórios.
      */
     class Awk_Path {
         /**
-         * Normaliza um caminho absoluto quando o `php:realpath()` não for capaz.
+         * Armazena o caminho.
+         * @var string
+         */
+        private $path;
+
+        /**
+         * Constrói uma nova instância.
+         * @param string $path Caminho do objeto.
+         */
+        public function __construct($path) {
+            $this->path = $path;
+        }
+
+        /**
+         * Retorna se o objeto é um arquivo.
+         * @return boolean
+         */
+        public function is_file() {
+            return is_file($this->path);
+        }
+
+        /**
+         * Retorna se o objeto é um diretório.
+         * @return boolean
+         */
+        public function is_dir() {
+            return is_dir($this->path);
+        }
+
+        /**
+         * Obtém o caminho armazenado no objeto.
+         * A informação é recebida sem normalização.
+         * @return string
+         */
+        public function get() {
+            return $this->path;
+        }
+
+        /**
+         * Retorna o caminho do objeto normalizado.
+         * @return string
+         */
+        public function get_normalized() {
+            return self::normalize($this->path);
+        }
+
+        /**
+         * Retorna o nome base do objeto.
+         * @return string
+         */
+        public function get_basename() {
+            return basename($this->path);
+        }
+
+        /**
+         * Retorna o diretório do objeto.
+         * @return string
+         */
+        public function get_dirname() {
+            return dirname($this->path);
+        }
+
+        /**
+         * Retorna a extensão do objeto.
+         * Retornará null se o objeto for um diretório.
+         * @return string|null
+         */
+        public function get_extension() {
+            if(is_file($this->path)) {
+                return pathinfo($this->path, PATHINFO_EXTENSION);
+            }
+        }
+
+        /**
+         * Retorna se um objeto existe.
+         * @return boolean
+         */
+        public function exists() {
+            return is_readable($this->path);
+        }
+
+        /**
+         * Normaliza um caminho absoluto, mesmo quando não houver um objeto real atingível.
          * @param  string $path Caminho a ser normalizado.
          * @return string
          */
@@ -15,6 +97,12 @@
             // Dados do resultado.
             $result_parts = [];
             $result_offset = 0;
+
+            // Indica que há uma barra inicial.
+            $result_slash = count($original_parts) > 0 && $original_parts[0] === "";
+            if($result_slash) {
+                array_shift($original_parts);
+            }
 
             // Valida cada parte do caminho.
             foreach($original_parts as $original_offset => $original_part) {
@@ -38,7 +126,16 @@
                 $result_offset+= $original_part !== "..";
             }
 
-            return rtrim(join("/", $result_parts), "/");
+            // Armazena o resultado.
+            $result = trim(join("/", $result_parts), "/");
+
+            // Adiciona a barra inicial, se houver.
+            if($result_slash) {
+                $result = "/" . $result;
+            }
+
+            // Retorna o resultado.
+            return $result;
         }
 
         /**
--- awk_suite/tests/Awk_PathTest.php
+++ awk_suite/tests/Awk_PathTest.php
@@ -5,34 +5,105 @@
      */
     class Awk_PathTest extends PHPUnit_Framework_TestCase {
         /**
-         * Testa com caminhos não reconhecidos.
-         * @return void
+         * Testa os métodos da instância em um diretório real.
          */
-        public function testNotRealPath() {
-            // Normalização de caminhos inexistentes.
-            $this->assertSame("abc", Awk_Path::normalize("abc/../abc"));
-            $this->assertSame("abc", Awk_Path::normalize("abc/../abc/"));
-            $this->assertSame("", Awk_Path::normalize("abc/.."));
-            $this->assertSame("", Awk_Path::normalize("abc/../"));
-            $this->assertSame("abc", Awk_Path::normalize("./abc/../abc"));
+        public function testDirectoryInstance() {
+            $path = new Awk_Path(getcwd());
+
+            $this->assertTrue($path->exists());
+            $this->assertTrue($path->is_dir());
+            $this->assertFalse($path->is_file());
+
+            $this->assertSame(getcwd(), $path->get());
         }
 
         /**
-         * Teste com caminhos reais.
-         * @return void
+         * Testa os métodos da instância em um arquivo real.
          */
-        public function testRealPath() {
-            $this->assertSame(str_replace("\\", "/", getcwd()), Awk_Path::normalize(getcwd()));
-            $this->assertSame(str_replace("\\", "/", getcwd()), Awk_Path::normalize(str_replace(DIRECTORY_SEPARATOR, "//", getcwd())));
+        public function testFileInstance() {
+            $path = new Awk_Path(getcwd() . "/../index.php");
+
+            $this->assertTrue($path->exists());
+            $this->assertTrue($path->is_file());
+            $this->assertFalse($path->is_dir());
+
+            $this->assertSame("index.php", $path->get_basename());
+            $this->assertSame("php", $path->get_extension());
         }
 
         /**
-         * Testa caminhos complexos, não reconhecidos.
-         * @return void
+         * Testa os métodos em uma instância artificial.
          */
-        public function testComplexPath() {
-            $this->assertSame("../abc", Awk_Path::normalize("../abc"));
-            $this->assertSame("../abc", Awk_Path::normalize("../abc/../abc"));
-            $this->assertSame("../../abc", Awk_Path::normalize("../abc/../../abc"));
+        public function testArtificialInstance() {
+            $path = new Awk_Path("/fake/dir");
+
+            $this->assertFalse($path->exists());
+            $this->assertFalse($path->is_file());
+            $this->assertFalse($path->is_dir());
+
+            $this->assertSame("dir", $path->get_basename());
+            $this->assertSame("/fake", $path->get_dirname());
+
+            $this->assertSame("/fake/dir", $path->get_normalized());
+
+            $this->assertNull($path->get_extension());
+        }
+
+        /**
+         * Teste de normalização em arquivos artificiais.
+         */
+        public function testArtificialNormalizations() {
+            $path = new Awk_Path("/abc/../abc");
+            $this->assertSame("/abc", $path->get_normalized());
+
+            $path = new Awk_Path("abc/../abc");
+            $this->assertSame("abc", $path->get_normalized());
+
+            $path = new Awk_Path("/abc/..");
+            $this->assertSame("/", $path->get_normalized());
+
+            $path = new Awk_Path("abc/..");
+            $this->assertSame("", $path->get_normalized());
+
+            $path = new Awk_Path("/abc/../");
+            $this->assertSame("/", $path->get_normalized());
+
+            $path = new Awk_Path("abc/../");
+            $this->assertSame("", $path->get_normalized());
+
+            $path = new Awk_Path("/./abc/../abc");
+            $this->assertSame("/abc", $path->get_normalized());
+
+            $path = new Awk_Path("./abc/../abc");
+            $this->assertSame("abc", $path->get_normalized());
+
+            $path = new Awk_Path("/../abc");
+            $this->assertSame("/../abc", $path->get_normalized());
+
+            $path = new Awk_Path("../abc");
+            $this->assertSame("../abc", $path->get_normalized());
+
+            $path = new Awk_Path("/../abc/../abc");
+            $this->assertSame("/../abc", $path->get_normalized());
+
+            $path = new Awk_Path("../abc/../abc");
+            $this->assertSame("../abc", $path->get_normalized());
+
+            $path = new Awk_Path("../abc/../../abc");
+            $this->assertSame("../../abc", $path->get_normalized());
+
+            $path = new Awk_Path("/../abc/../../abc");
+            $this->assertSame("/../../abc", $path->get_normalized());
+        }
+
+        /**
+         * Teste de normalização em caminhos reais.
+         */
+        public function testRealNormalizations() {
+            $path = new Awk_Path(getcwd());
+            $this->assertSame(str_replace("\\", "/", getcwd()), $path->get_normalized());
+
+            $path = new Awk_Path(str_replace(DIRECTORY_SEPARATOR, "//", getcwd()));
+            $this->assertSame(str_replace("\\", "/", getcwd()), $path->get_normalized());
         }
     }
